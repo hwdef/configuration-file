c52f5d4ba99306175b98a6c9e559e8d098975446
LX <hwdefcom@outlook.com>
2023-08-06 04:57:57 +0000

    add wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'OneHalfDark'
+config.font = wezterm.font 'intelone mono'
+config.font_size = 19
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 50
+config.native_macos_fullscreen_mode = true
+config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+-- config.color_scheme = 'Ayu Mirage'
+-- config.color_scheme = 'OneHalfDark'
+config.color_scheme = 'OneHalfLight'
+-- and finally, return the configuration to wezterm
+return config
